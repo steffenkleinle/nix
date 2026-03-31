4f2851ad8a4b5fd48a428f50b5ae852d66e51f02
Steffen Kleinle <steffen.kleinle@mailbox.org>
2026-03-31 11:54:10 +0200

    Remove balenaetcher

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
         enable = true;
         taps = [ "leoafarias/fvm" "th-ch/youtube-music" ];
         brews = [ "bundletool" "circleci" "cocoapods" "fvm" "gnupg" "qemu" "ruby-build" ];
-        casks = [ "balenaetcher" "ferdium" "libreoffice" "mullvad-vpn" "standard-notes" "stremio" "youtube-music" ];
+        casks = [ "ferdium" "libreoffice" "mullvad-vpn" "standard-notes" "stremio" "youtube-music" ];
         onActivation = {
           autoUpdate = true;
           cleanup = "uninstall";
